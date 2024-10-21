e59cecb58e4e49940a8d2cc5c669a0e27250d907
maestraveronicamartinez <veronicamartinez@cbtis89.edu.mx>
2024-10-21 09:31:03 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,13 @@
+<?php
+/* CBTIS 89 
+   P00Presentación
+   Programa que contiene mis datos personales
+   Verónica Martínez Anaya
+   3ºA Programación Matutino */
+
+   echo "Mi nombre es Verónica Martínez Anaya";
+   echo "<br>","Estudio en el CBTIS 89";
+   echo "<br>","En 3er semestre de la carrera de Programación A Matutino"; 
+   echo "<br>", "Vivo en la ciudad de Durango, Dgo.";
+
+   ?>
